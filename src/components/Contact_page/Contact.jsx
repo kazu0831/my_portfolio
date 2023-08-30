8d0c067b593ceb533ba5c4a002778668cfa66c02
kasu0831 <kukuaice@gmail.com>
2023-08-30 11:07:42 +0900

    modify contact top

--- src/components/Contact_page/Contact.jsx
+++ src/components/Contact_page/Contact.jsx
@@ -3,8 +3,15 @@ import { Form } from './Form'
 
 function Contact() {
   return (
+
     <div>
+
+      <div className='py-6 pb-12 flex justify-center'>
+        <h2 className='text-4xl text-white font-bold'>Contact</h2>
+      </div>
+
       <Form />
+
     </div>
   )
 }
--- src/components/Top_page/Home.jsx
+++ src/components/Top_page/Home.jsx
@@ -6,16 +6,16 @@ import Objects from './Objects'
 function Home() {
   return (
 
-    <div className='flex bg-gradient-to-r from-blue-500 to-purple-500'>
+    <div className='flex bg-gradient-to-r from-blue-500 to-purple-500 h-screen-minus-60'>
       
-      <div className='flex flex-col justify-center items-center w-1/2 h-screen'>
+      <div className='flex flex-col justify-center items-center w-1/2'>
 
         <Title />
         <Subtitle text="I'm Kazu / passinate about front-end" delay={100}/>
         
       </div>
 
-      <div className='flex flex-col justify-center items-center w-1/2 h-screen'>
+      <div className='flex flex-col justify-center items-center w-1/2'>
 
         <Objects />
         
